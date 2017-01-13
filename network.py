e0f9c1888fd1a292e401f27679964f98842a97d0
MWransky <michael.wransky@udacity.com>
2017-01-12 21:58:43 -0500

    use a three branched model with present image, past image, and past angle as inputs

--- network.py
+++ network.py
@@ -17,87 +17,73 @@ def custom_init(shape, name=None):
     return normal(shape, scale=0.01, name=name)
 
 
-def build_network(input_shape=[160, 320, 1]):
+def build_network(input_shape=[40, 80, 1]):
     # input_shape is [n_rows, n_cols, num_channels] of the input images
     activation = 'relu'
 
     nrows, ncols, nchannels = input_shape
 
-    model = Sequential()
-    # model.add(Activation(activation=intensity_norm, input_shape=(nrows, ncols, nchannels)))
+    past_image = Sequential()
+    # past_image.add(Activation(activation=intensity_norm, input_shape=(nrows, ncols, nchannels)))
 
     # First three convolutional layers with kernal size 5x5 and stride 2x2
-    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), border_mode='valid', init='normal', activation=activation, input_shape=(nrows, ncols, nchannels)))
-    model.add(Dropout(0.3))
-    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), border_mode='valid', init='normal', activation=activation))
-    model.add(Dropout(0.3))
-    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), border_mode='valid', init='normal', activation=activation))
-    model.add(Dropout(0.3))
-    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2), border_mode='same'))
+    past_image.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode='valid', init='normal', activation=activation, input_shape=(nrows, ncols, nchannels)))
+    past_image.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1), border_mode='same'))
+    past_image.add(Dropout(0.3))
+    # past_image.add(Convolution2D(36, 5, 5, subsample=(2, 2), border_mode='valid', init='normal', activation=activation))
+    past_image.add(Convolution2D(48, 5, 5, subsample=(2, 2), border_mode='valid', init='normal', activation=activation))
+    past_image.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2), border_mode='same'))
 
     # Final two convolutional layers with 3x3 kernal and no stride
-    model.add(Convolution2D(64, 3, 3, border_mode='valid', subsample=(1, 1), init='normal', activation=activation))
-    model.add(Dropout(0.3))
-    model.add(Convolution2D(64, 3, 3, border_mode='valid', subsample=(1, 1), init='normal', activation=activation))
-    model.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1), border_mode='same'))
-    model.add(Dropout(0.3))
+    past_image.add(Convolution2D(64, 3, 3, border_mode='valid', subsample=(1, 1), init='normal', activation=activation))
+    past_image.add(Dropout(0.3))
+    # past_image.add(Convolution2D(64, 3, 3, border_mode='valid', subsample=(1, 1), init='normal', activation=activation))
+    past_image.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1), border_mode='same'))
 
     # Flatten convolition layers
-    model.add(Flatten())
-    model.add(Dense(100, init='normal', activation=activation))
-    model.add(Dropout(0.4))
-    model.add(Dense(50, init='normal', activation=activation))
+    past_image.add(Flatten())
+    past_image.add(Dense(10, init='normal', activation=activation))
+    past_image.add(Dropout(0.3))
+    past_image.add(Dense(5, init='normal', activation=activation))
+    past_image.add(Dense(1))
+
+    present_image = Sequential()
+    # present_image.add(Activation(activation=intensity_norm, input_shape=(nrows, ncols, nchannels)))
+
+    # First three convolutional layers with kernal size 5x5 and stride 2x2
+    present_image.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode='valid', init='normal', activation=activation, input_shape=(nrows, ncols, nchannels)))
+    present_image.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1), border_mode='same'))
+    present_image.add(Dropout(0.3))
+    # present_image.add(Convolution2D(36, 5, 5, subsample=(2, 2), border_mode='valid', init='normal', activation=activation))
+    present_image.add(Convolution2D(48, 5, 5, subsample=(2, 2), border_mode='valid', init='normal', activation=activation))
+    present_image.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2), border_mode='same'))
+
+    # Final two convolutional layers with 3x3 kernal and no stride
+    present_image.add(Convolution2D(64, 3, 3, border_mode='valid', subsample=(1, 1), init='normal', activation=activation))
+    present_image.add(Dropout(0.3))
+    # present_image.add(Convolution2D(64, 3, 3, border_mode='valid', subsample=(1, 1), init='normal', activation=activation))
+    present_image.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1), border_mode='same'))
+
+    # Flatten convolition layers
+    present_image.add(Flatten())
+    present_image.add(Dense(10, init='normal', activation=activation))
+    present_image.add(Dropout(0.3))
+    present_image.add(Dense(5, init='normal', activation=activation))
+    present_image.add(Dense(1))
+
+    # Model branch using past angle as input
+
+    past_angle = Sequential()
+
+    past_angle.add(Dense(512, init='normal', activation='tanh', input_dim=1))
+    past_angle.add(Dropout(0.3))
+    past_angle.add(Dense(32, init='normal', activation='tanh'))
+    past_angle.add(Dense(1))
+
+    # Combine three model branches into one
+
+    model = Sequential()
+    model.add(Merge([past_image, present_image, past_angle], mode='concat'))
     model.add(Dense(1))
-
-    # model.add(Dense(1000, activation=activation))
-    # model.add(Dense(500, activation=activation))
-    # model.add(Dense(n_classes, init='he_normal', activation='softmax'))
-
-    # Three fully connected layers
-    # model.add(Dense(100, activation=activation))
-    # model.add(Dropout(0.5))
-    # model.add(Dense(50, activation=activation))
-    # model.add(Dropout(0.5))
-    # model.add(Dense(10, activation=activation))
-    # model.add(Dropout(0.5))
-    # model.add(Dense(n_classes))
-    # model.add(Convolution2D(dim, 3, 3, activation=activation))
-    # model.add(Dropout(0.2))
-
-    # model.add(BatchNormalization())
-
-    # model.add(Convolution2D(64, 3, 3, activation=activation))
-    # model.add(BatchNormalization())
-    # model.add(Dropout(0.3))
-
-    # model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2), border_mode='same'))
-
-    # model.add(Convolution2D(256, 3, 3, activation=activation))
-    # model.add(BatchNormalization())
-    # model.add(Dropout(0.3))
-
-    # model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2), border_mode='same'))
-
-    # model.add(Convolution2D(256, 3, 3, activation=activation))
-    # model.add(BatchNormalization())
-    # model.add(Dropout(0.3))
-
-    # for i in range(2):
-    #     model.add(_highway())
-    #     model.add(BatchNormalization())
-    #     model.add(Dropout(0.5))
-
-    # for i in range(2):
-    #     model.add(_highway())
-    #     model.add(Dropout(0.5))
-    #     model.add(BatchNormalization())
-
-    # model.add(Dropout(0.3))
-
-    # model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2), border_mode='same'))
-
-    # model.add(Flatten())
-    # model.add(Dense(256, activation=activation))
-    # model.add(Dense(n_classes))
 
     return model
